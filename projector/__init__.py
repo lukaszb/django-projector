f0dbf34342d4291e58b8d8a002c950a35fc25614
Lukasz Balcerzak <lukasz.balcerzak@python-center.pl>
2010-03-15 11:57:12 +0100

    Increasing version number.

--- projector/__init__.py
+++ projector/__init__.py
@@ -3,7 +3,7 @@ Project management Django application with task
 tracker and repository backend integration.
 """
 
-VERSION = (0, 1, 4, 'beta')
+VERSION = (0, 1, 5, 'beta')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
