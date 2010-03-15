860b4ca684b7d042838883c52031de0cf3d8b2ef
Lukasz Balcerzak <lukasz.balcerzak@python-center.pl>
2010-03-15 12:01:42 +0100

    Bumping version number.

--- projector/__init__.py
+++ projector/__init__.py
@@ -3,7 +3,7 @@ Project management Django application with task
 tracker and repository backend integration.
 """
 
-VERSION = (0, 1, 5, 'beta')
+VERSION = (0, 1, 6, 'beta')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
