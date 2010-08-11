e9e0b9b601e18a158830fb654647367293a2e9c4
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2010-08-11 23:32:35 +0200

    Removed BaseView, now View is base class-view; added __before__ and __after__ methods

--- projector/core/controllers.py
+++ projector/core/controllers.py
@@ -1,49 +1,92 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 login_required_m = method_decorator(login_required)
 
-class BaseView(object):
+class View(object):
     """
-    Base class for django views.
+    Class-based view allowing it's subclasses to return dictionary at
+    ``response`` method. :py:class:`django.http.HttpResponse` may also be
+    returned, as usual.
     """
     login_required = False
-    template_name = ''
+    template_name = 'base.html'
 
     def __new__(cls, request, *args, **kwargs):
+        # Wraps response method with ``login_required`` decorator
+        # if ``login_required`` attribute is set to ``True`` at the class
         if cls.login_required:
             cls.response = login_required_m(cls.response)
         view = cls.new(request, *args, **kwargs)
+
+        # If already a HttpResponse we should propagate it
+        if isinstance(view, HttpResponse):
+            return view
         return view.__call__()
 
     @classmethod
     def new(cls, request, *args, **kwargs):
         obj = object.__new__(cls)
         if not isinstance(request, HttpRequest):
-            raise TypeError("Class based views requires HttpRequest "
-                "to be passed as first argument (got %s)" % request)
+            raise TypeError(
+            "Class based views requires HttpRequest to be passed as first "
+            "argument (got %s)" % request)
         obj.request = request
-        obj.context = {}
-        obj.__init__(request, *args, **kwargs)
         obj.args = args
         obj.kwargs = kwargs
+        obj.context = {}
+        # Call __before__ method
+        before = obj.__before__()
+        if isinstance(before, HttpResponse):
+            return before
+        obj.__init__(request, *args, **kwargs)
         return obj
+
+    def __before__(self):
+        """
+        Method called just after class is created and all passed parameters are
+        set on the class, so it is possible to access those attributes.
+
+        If this method would return :py:class:`django.http.HttpResponse`
+        instance, it would be propagated.
+        """
+        pass
+
+    def __after__(self):
+        """
+        Method called at the final step of the view-class processing. If
+        :py:class:`django.http.HttpResponse` instance is returned it is
+        propagated to the view handler.
+
+        .. note::
+           Adding i.e. ``context`` at this stage is pointless - response
+           should have been already prepared and changing ``context`` wouldn't
+           change the response. ``__after__`` method should be implemented
+           for other purposes, like checking status code of prepared response.
+           Response object is available by accessing ``self._response``. This
+           attribute is available **only** at this stage.
+        """
+        pass
 
     def __init__(self, request, *args, **kwargs):
         """
         May be overridden, for example::
 
             def __init__(self, request, page_slug):
-                self.request = request
                 self.page_slug = page_slug
 
         """
         pass
 
     def __call__(self):
+        """
+        Should be overridden only for special cases as it runs ``__after__``
+        method. By subclassing it is possible to change the order of method
+        calls.
+        """
         if self.args and self.kwargs:
             response = self.response(self.request, *self.args, **self.kwargs)
         elif self.args:
@@ -52,23 +95,20 @@ class BaseView(object):
             response = self.response(self.request, **self.kwargs)
         else:
             response = self.response(self.request)
-        return response
 
-    def response(self, request):
-        raise NotImplementedError("BaseView subclasses should implement "
-            "__call__ method (it should return django.http.HttpResponse)")
-
-class View(BaseView):
-
-    def __call__(self):
-
-        response = super(View, self).__call__()
-
+        # If response is a dict, then we have to trasform it into HttpResponse
         if isinstance(response, dict):
-            return render_to_response(self.template_name, response,
+            response = render_to_response(self.template_name, response,
                 RequestContext(self.request))
+        self._response = response
+
+        # Call __after__ method just before we return response
+        after = self.__after__()
+        if isinstance(after, HttpResponse):
+            return after
+        del self._response
         return response
 
     def response(self, request):
-        return {}
+        return self.context
 
--- projector/tests/test_controllers.py
+++ projector/tests/test_controllers.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.test.client import Client
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import AnonymousUser
 
 from projector.core.controllers import View
 
@@ -7,11 +9,12 @@ class ViewTest(TestCase):
 
     def setUp(self):
         self.request = HttpRequest()
+        self.request.user = AnonymousUser()
 
     def test_response(self):
 
         class AnyView(View):
-            template_name = 'tests/base.html'
+            template_name = 'tests/dummy.html'
 
         response = AnyView(self.request)
         self.assertTrue(isinstance(response, HttpResponse))
@@ -19,11 +22,101 @@ class ViewTest(TestCase):
     def test_dict_response(self):
 
         class DictView(View):
-            template_name = 'tests/base.html'
+            template_name = 'tests/dummy.html'
             def response(self, request):
-                return {}
+                return self.context
 
         response = DictView(self.request)
         self.assertTrue(isinstance(response, HttpResponse))
 
+    def test_login_required(self):
+
+        class LoginRequiredView(View):
+            template_name = 'tests/dummy.html'
+            login_required = True
+            def response(self, request):
+                return self.context
+
+        self.request.user = AnonymousUser()
+        response = LoginRequiredView(self.request)
+        self.assertEqual(response.status_code, 302)
+
+    def test_before(self):
+
+        test = self
+
+        class BeforeView(View):
+            template_name = 'tests/dummy.html'
+            def __before__(self):
+                self.context['before'] = 'before'
+            def response(self, request):
+                test.assertEqual(self.context['before'], 'before')
+
+        self = test
+        # Assertion is called inside response method at the BeforeView
+        BeforeView(self.request)
+
+    def test_before_response(self):
+
+        class BeforeResponseView(View):
+            template_name = 'tests/dummy.html'
+            def __before__(self):
+                return HttpResponse(content='foobar', status=500)
+            def response(self, request):
+                return HttpResponse(status=200)
+
+        response = BeforeResponseView(self.request)
+        self.assertEqual(response.status_code, 500)
+
+    def test_after(self):
+
+        test = self
+
+        class AfterView(View):
+            template_name = 'tests/dummy.html'
+            def __after__(self):
+                self.context['after'] = 'after'
+                if self._response.status_code == 200:
+                    self._response.status_code = 501
+            def response(self, request):
+                test.assertFalse('after' in self.context)
+                return self.context
+
+        self = test
+        # Assertion is called inside response method at AfterView
+        AfterView(self.request)
+
+    def test_after_response(self):
+
+        class AfterResponseView(View):
+            template_name = 'tests/dummy.html'
+            def __after__(self):
+                return HttpResponse(content='foobar', status=500)
+            def response(self, request):
+                return HttpResponse(status=200)
+
+        response = AfterResponseView(self.request)
+        self.assertEqual(response.status_code, 500)
+
+    def test_before_after_mix(self):
+
+        test = self
+
+        class BeforeAndAfterView(View):
+            template_name = 'tests/dummy.html'
+            def __before__(self):
+                self.context['before'] = 'before'
+                self.context['mix'] = 'before'
+            def __after__(self):
+                self.context['after'] = 'after'
+                self.context['mix'] = 'after'
+            def response(self, request):
+                test.assertEqual(self.context['before'], 'before')
+                test.assertEqual(self.context['mix'], 'before')
+                test.assertFalse('after' in self.context)
+                return self.context
+
+        self = test
+        # Assertions are called inside response method at BeforeAndAfterView
+        BeforeAndAfterView(self.request)
 
