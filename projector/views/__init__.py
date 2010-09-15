cd957e9df0ca854793a85f082a5b69868f6c8dd1
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2010-09-15 02:16:58 +0200

    Removed livesettings specific 'settings' view

--- projector/views/__init__.py
+++ projector/views/__init__.py
@@ -1,21 +1,9 @@
 from django.views.generic import simple
-from django.http import Http404
 
 def home(request, template_name='projector/home.html'):
+    """
+    Home view for projector app.
+    """
 
     return simple.direct_to_template(request, {'template': template_name})
-
-def settings(request):
-    """
-    Wrapper for standard livesettings view allowing only superusers to make
-    projector-wide settings changes.
-    If not requested by superuser 404 would be returned - not 403 as we don't
-    want to expose this page to anyone at all.
-    """
-    from livesettings.views import group_settings
-
-    if not (request.user.is_authenticated() and request.user.is_superuser):
-        raise Http404
-    return group_settings(request, 'PROJECTOR',
-        template='projector/settings.html')
 
